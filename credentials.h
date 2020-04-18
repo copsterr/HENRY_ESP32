bb27e0ced58ec500a146d82e3282366df7e4f892
Siraphob Kanjanasaensong <8vxg9viN>
2020-04-18 14:08:14 +0700

    add md

--- credentials.h
+++ credentials.h
@@ -1,6 +1,16 @@
+/* You can change wifi and password here
+ It is recommended to use this device with a WiFi router.
+ Using mobile hotspot raise unstable connection and can't be used with webservice
+ Oh! One thing! It is unable to connect to enterprise wifi. I know you guys may wonder how wifi are different?
+ It is different. Just try to use with a simple personal router wifi at home.
+*/
+
+// Edit WiFi credentials here
 #define WIFI_SSID "CHAILEE_2_2.4GHz"
 #define WIFI_PASS "phaen71214"
 
+
+// NEVER EVER CHANGE THESE BELOW LINES PLEASE
 #define APPID   "MookSqueezer"
 #define KEY     "y2kfKhLcDuVcR7a"
 #define SECRET  "wGFmkwimlijcNopWCPvQsULTb"
