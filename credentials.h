00077b913f632c091c016d6b0bb3e0c8a1eaa37e
Siraphob Kanjanasaensong <8vxg9viN>
2020-04-18 13:47:25 +0700

    stable release 1.0.0

--- credentials.h
+++ credentials.h
@@ -1,6 +1,11 @@
+// You can change wifi and password here
+// It is recommended to use this device with a WiFi router.
+// Using mobile hotspot raise unstable connection and can't be used with webservice
 #define WIFI_SSID "CHAILEE_2_2.4GHz"
 #define WIFI_PASS "phaen71214"
 
+
+// NEVER EVER CHANGE THESE BELOW LINES PLEASE
 #define APPID   "MookSqueezer"
 #define KEY     "y2kfKhLcDuVcR7a"
 #define SECRET  "wGFmkwimlijcNopWCPvQsULTb"
